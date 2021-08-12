1f7dfbf1eac67a58ebb789fe0e9a4aff213e1e5a
Nuevee <cv4283@innovaccion.mx>
2021-08-12 17:55:08 -0500

    corregido editar clientes

--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class clientes extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $table = 'clientes';
 
     protected $primaryKey = 'id_cliente';
